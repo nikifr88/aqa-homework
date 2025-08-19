d6455022eb22176ae2dee5601c2651ae113b7350
Nikita <nikifr.kh@gmail.com>
2025-08-19 08:25:12 +0300

    create homework with functions

---
+++ intro-to-functions/task1.js
@@ -0,0 +1,18 @@
+// function declaration
+function areaRectangleDec(width, height) {
+    return width * height;
+}
+
+console.log(areaRectangleDec(5, 10));
+
+//function expression
+const areaRectangleExp = function(width, height) {
+    return width * height;
+}
+
+console.log(areaRectangleExp(6, 11))
+
+//arrow function
+const areaRectangleArr = (width, height) => width * height;
+
+console.log(areaRectangleArr(7, 12));
