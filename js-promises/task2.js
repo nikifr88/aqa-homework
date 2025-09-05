2bc0a5cd33589c7a5bf6fe3df526434264885406
Nikita <nikifr.kh@gmail.com>
2025-09-05 09:52:41 +0300

    do homework js-promises

---
+++ js-promises/task2.js
@@ -0,0 +1,40 @@
+function getTodos() {
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(() => reject('Ответ не пришел'));
+    })
+}
+
+function getUser() {
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/users/1')
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(() => reject('Ответ не пришел'));
+    })
+}
+
+let resultAll = [];
+let resultRace;
+
+Promise.all([getTodos(), getUser()])
+    .then(arr => {
+        resultAll.push([...arr]);
+        console.log('ResultAll: ', resultAll);
+    })
+    .catch(() => {
+        resultAll.push('Ни один из запросов не выполнился');
+        console.log('ResultAll: ', resultAll);
+    });
+
+Promise.race([getTodos(), getUser()])
+    .then(data => {
+        resultRace = data;
+        console.log('ResultRace: ', resultRace);
+    })
+    .catch(() => {
+        resultRace = 'Ни один из запросов не выполнился';
+        console.log('ResultRace: ', resultRace);
+    });
---
+++ js-promises/task4.js
@@ -0,0 +1,46 @@
+class GetTodos {    
+    static async getTodosAsync() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw 'Ответ не пришел'
+        }
+    }
+}
+
+class GetUser {
+    static async getUserAsync() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw 'Ответ не пришел'
+        }
+    }
+}
+
+let resultAll = [];
+let resultRace;
+
+Promise.all([GetTodos.getTodosAsync(), GetUser.getUserAsync()])
+    .then(arr => {
+        resultAll.push([...arr]);
+        console.log('ResultAll: ', resultAll);
+    })
+    .catch(() => {
+        resultAll.push('Ни один из запросов не выполнился');
+        console.log('ResultAll: ', resultAll);
+    });
+
+Promise.race([GetTodos.getTodosAsync(), GetUser.getUserAsync()])
+    .then(data => {
+        resultRace = data;
+        console.log('ResultRace: ', resultRace);
+    })
+    .catch(() => {
+        resultRace = 'Ни один из запросов не выполнился';
+        console.log('ResultRace: ', resultRace);
+    });
